cf2d1643101c3db9e9b4dec5899682bb211e56d1
gumball1 <16063498+gumball1@users.noreply.github.com>
2025-04-21 22:15:40 +0100

    updt

---
+++ repo/service.subtitles.napiprojektkatalog/resources/lib/NapiProjekt.py
@@ -0,0 +1,286 @@
+# *-* coding: utf-8 *-*
+
+import base64
+import re
+import traceback
+from urllib.parse import urljoin
+from xml.dom import minidom
+
+import dom_parser
+import requests
+import xbmc
+import xbmcaddon
+import PTN
+
+__addon__ = xbmcaddon.Addon()
+__scriptname__ = __addon__.getAddonInfo('name')
+
+
+class NapiProjektKatalog:
+    def __init__(self):
+        self.download_url = "http://napiprojekt.pl/api/api-napiprojekt3.php"
+        self.base_url = "http://www.napiprojekt.pl"
+        self.search_url = "/ajax/search_catalog.php"
+
+    def log(self, msg=None, ex=None):
+        if ex:
+            level = xbmc.LOGERROR
+            msg = traceback.format_exc()
+        else:
+            level = xbmc.LOGINFO
+
+        xbmc.log((u"### [%s] - %s" % (__scriptname__, msg)), level=level)
+
+    def notify(self, msg):
+        xbmc.executebuiltin((u'Notification(%s,%s)' % (__scriptname__, msg)))
+
+    def try_get_org_title(self, title):
+        start_index = title.find("(")
+        end_index = title.find(")")
+        if start_index != -1 and end_index != -1:
+            return title[start_index + 1:end_index]
+        return None
+
+    def find_subtitle_page(self, item, counter = 0):
+        if not any((True for x in item["3let_language"] if x in ["pl", "pol"])):
+            self.log('Only polish supported')
+            self.notify('Only Polish supported')
+            return None
+
+        if item['tvshow'] or ('showTraktId' in item["file_original_path"] and 'showTraktId=null' not in item["file_original_path"]):
+            if 'showTraktId' in item["file_original_path"]:
+                showTraktId = re.findall("showTraktId=(.*?)&", item["file_original_path"])[0]
+                title_to_find = requests.get("https://trakt.tv/shows/%s" % showTraktId, allow_redirects=False).headers['location'].split("/")[-1].replace("-", " ")
+                query_kind = 1
+                query_year = re.findall(r"\d{4}", title_to_find)[-1]
+            else:
+                title_to_find = item['tvshow']
+                query_kind = 1
+                query_year = ''
+        else:
+            if 'movieTraktId' in item["file_original_path"]:
+                movieTraktId = re.findall("movieTraktId=(.*?)&", item["file_original_path"])[0]
+                title_to_find = requests.get("https://trakt.tv/movies/%s" % movieTraktId, allow_redirects=False).headers['location'].split("/")[-1].replace("-", " ")
+                query_kind = 2
+                query_year = re.findall(r"\d{4}", title_to_find)[-1]
+            else:
+                parsed = PTN.parse(item['title'])
+                try:
+                    title_to_find = parsed['title']
+                except:
+                    title_to_find = item['title']
+                try:
+                    query_year = str(parsed['year'])
+                except:
+                    query_year = item['year']
+                query_kind = 2
+
+        post = {'queryKind': query_kind,
+                'queryString': self.getsearch(title_to_find),
+                'queryYear': query_year,
+                'associate': ''}
+        self.log('searching for movie: ' + str(post))
+        # post = urllib.parse.urlencode(post, doseq=True)
+
+        url = self.base_url + self.search_url
+        subs = requests.post(url, data=post).text
+        rows = self.parseDOM_base(subs, 'a', attrs={'class': 'movieTitleCat'})
+
+        clean_title = self.get_clean(title_to_find)
+        for row in rows:
+            title = self.parseDOM(row.content, 'h3')[0]
+            if not title:
+                title = row.attrs['tytul']
+            words = clean_title.lower().split(" ")
+            if self.contains_all_words(self.get_clean(title), words):
+                self.log('Found: ' + title)
+                result = urljoin(self.base_url, row.attrs['href'])
+                if item['tvshow'] or ('showTraktId' in item["file_original_path"] and 'showTraktId=null' not in item["file_original_path"]):
+                    if 'seasonNumber' in item["file_original_path"]:
+                        season = re.findall("seasonNumber=(.*?)&", item["file_original_path"])[0]
+                        episode = re.findall("episodeNumber=(.*)", item["file_original_path"])[0]
+                    else:
+                        season = item['season']
+                        episode = item['episode']
+                    result += '-s' + season.zfill(2) + 'e' + episode.zfill(2)
+                result = result.replace('napisy-', 'napisy1,1,1-dla-', 1).encode('utf-8')
+                return result
+            elif counter == 0:
+                if item['title'] == item['videoplayer_title']:
+                    if item['tvshow']:
+                        response = requests.get("https://www.filmweb.pl/serials/search?q=%s" % self.getsearch(title_to_find).replace(" ", "%20")).text
+                        if(len(re.findall("filmPreview__title\">(.*?)<", response))>0):
+                            alternative_title = re.findall("filmPreview__title\">(.*?)<", response)[0]
+                            item.update({'videoplayer_title': alternative_title})
+                        else:
+                            item.update({'videoplayer_title': title})
+                    else:
+                        response = requests.get("https://www.filmweb.pl/films/search?q=%s" % self.getsearch(title_to_find).replace(" ", "%20")).text
+                        if(len(re.findall("filmPreview__title\">(.*?)<", response))>0):
+                            alternative_title = re.findall("filmPreview__title\">(.*?)<", response)[0]
+                            item.update({'videoplayer_title': alternative_title})
+                        else:
+                            item.update({'videoplayer_title': title})
+                    
+                counter += 1
+                print(item['videoplayer_title'])
+                item.update({'title': item['videoplayer_title']})
+                result = self.find_subtitle_page(item,counter)
+                return result
+
+    def _is_synced(self, item, video_file_size, video_time=0):
+        import datetime
+        import time
+        sync = False
+        try:
+            totalTime = float(xbmc.Player().getTotalTime())
+
+            x = time.strptime(str(video_time).split('.')[0], '%H:%M:%S')
+            milliseconds = float(float(str(video_time).split('.')[1]) / 1000)
+            video_time = int(
+                datetime.timedelta(hours=x.tm_hour, minutes=x.tm_min, seconds=x.tm_sec).total_seconds()) + milliseconds
+            if abs(totalTime - video_time) <= 0.5:
+                sync = True
+
+            if len(video_file_size) > 0:
+                video_file_size = float(re.findall(r"([\d.]+)", video_file_size)[0])
+                file_size = round(item["file_original_size"] / float(1048576), 2)
+                if abs(file_size - video_file_size) <= 2:
+                    sync = True
+        except:
+            return sync
+
+        return sync
+
+    def search(self, item):
+        subtitle_list = []
+        self.log(item)
+        self.log("B:")
+        self.log(self.find_subtitle_page(item))
+        try:
+            url = self.find_subtitle_page(item).decode('utf-8')
+            if not url:
+                self.notify('Movie not found')
+                return subtitle_list
+            self.log('trying to get subtitles list')
+            page = requests.get(url.replace("napisy1,1,1", "napisy1,7,0")).text
+            page = self.parseDOM(page, 'tbody')
+            if len(page) > 0:
+                rows = self.parseDOM(page, 'tr')
+                for row in rows:
+                    link_hash = self.parseDOM(row, 'a', ret='href')[0]
+                    link_hash = link_hash.replace('napiprojekt:', '')
+                    cols = self.parseDOM(row, 'p')
+                    cols.pop(0)
+                    label = ' | '.join(cols[:-1])
+                    try:
+                        int(cols[5])
+                    except:
+                        continue
+                    subtitle_list.append({'language': 'pol',
+                                          'label': label,
+                                          'link_hash': link_hash,
+                                          'sync': self._is_synced(item, cols[0], cols[2]),
+                                          'data': cols[4],
+                                          'downloads': int(cols[5])
+                                          })
+            else:
+                self.notify('No subtitles available')
+        except Exception as e:
+            self.notify('Search error, check log')
+            self.log(ex=e)
+
+        return sorted(subtitle_list, key=lambda x: (x['sync'], x['downloads']), reverse=True)
+
+    def get_clean(self, title):
+        if title is None: return
+        title = re.sub('&#(\d+);', '', title)
+        title = re.sub('(&#[0-9]+)([^;^0-9]+)', '\\1;\\2', title)
+        title = title.replace('&quot;', '\"').replace('&amp;', '&')
+        title = title.replace('&', 'and')
+        title = re.sub(r'\n|([[].+?[]])|([(]\d.*?[)])|\s(vs|v[.])\s|(:|;|-|–|"|,|\'|\_|\.|\?)', '', title).lower()
+        if title.startswith('the'):
+            title = title[3:]
+        return self.normalize(title)
+
+    def contains_word(self, str_to_check, word):
+        if str(word).lower() in str(str_to_check).lower():
+            return True
+        return False
+
+    def contains_all_words(self, str_to_check, words):
+        for word in words:
+            if not self.contains_word(str_to_check, word):
+                return False
+        return True
+
+    def parseDOM_base(self, html, name, attrs):
+        if attrs:
+            attrs = dict((key, re.compile(value + ('$' if value else ''))) for (key, value) in attrs.items())
+        results = dom_parser.parse_dom(html, name, attrs)
+        return results
+
+    def parseDOM(self, html, name='', attrs=None, ret=False):
+        results = self.parseDOM_base(html, name, attrs)
+        if ret:
+            results = [result.attrs[ret.lower()] for result in results]
+        else:
+            results = [result.content for result in results]
+        return results
+
+    def getsearch(self, title):
+        if title is None: return
+        title = title.lower()
+        title = re.sub('&#(\d+);', '', title)
+        title = re.sub('(&#[0-9]+)([^;^0-9]+)', '\\1;\\2', title)
+        title = title.replace('&quot;', '\"').replace('&amp;', '&')
+        title = re.sub('\\\|/|-|–|:|;|\*|\?|"|\'|<|>|\|', '', title).lower()
+        return title
+
+    def normalize(self, title):
+        title = str(title).lower() \
+            .replace('ą', 'a') \
+            .replace('ę', 'e') \
+            .replace('ć', 'c') \
+            .replace('ź', 'z') \
+            .replace('ż', 'z') \
+            .replace('ó', 'o') \
+            .replace('ł', 'l') \
+            .replace('ń', 'n') \
+            .replace('ś', 's')
+        return title
+
+    def download(self, md5hash, filename, language="PL"):
+        try:
+            values = {
+                "mode": "1",
+                "client": "NapiProjektPython",
+                "client_ver": "0.1",
+                "downloaded_subtitles_id": md5hash,
+                "downloaded_subtitles_txt": "1",
+                "downloaded_subtitles_lang": language
+            }
+
+            self.log('Downloading subs: ' + str(values))
+
+            # data = urllib.parse.urlencode(values)
+
+            response = requests.post(self.download_url, data=values).text
+
+            DOMTree = minidom.parseString(response)
+
+            cNodes = DOMTree.childNodes
+            if cNodes[0].getElementsByTagName("status"):
+                text = base64.b64decode(
+                    cNodes[0].getElementsByTagName("subtitles")[0].getElementsByTagName("content")[0].childNodes[
+                        0].data)
+                filename = filename[:filename.rfind(".")] + ".txt"
+                open(filename, "wb").write(text)
+                return filename
+
+        except Exception as e:
+            self.notify('Download error, check log')
+            self.log(ex=e)
+            pass
+
+        return None
---
+++ repo/service.subtitles.napiprojektkatalog/service.py
@@ -0,0 +1,257 @@
+# -*- coding: utf-8 -*- 
+
+import os
+import re
+import shutil
+import sys
+import urllib
+from urllib.parse import unquote
+
+import unicodedata
+import xbmc
+import xbmcaddon
+import xbmcgui
+import xbmcplugin
+import xbmcvfs
+
+__addon__ = xbmcaddon.Addon()
+__scriptid__ = __addon__.getAddonInfo('id')
+__scriptname__ = __addon__.getAddonInfo('name')
+__version__ = __addon__.getAddonInfo('version')
+__language__ = __addon__.getLocalizedString
+
+__cwd__ = xbmcvfs.translatePath(__addon__.getAddonInfo('path'))
+__profile__ = xbmcvfs.translatePath(__addon__.getAddonInfo('profile'))
+__resource__ = xbmcvfs.translatePath(os.path.join(__cwd__, 'resources', 'lib'))
+__temp__ = xbmcvfs.translatePath(os.path.join(__profile__, 'temp', ''))
+
+sys.path.append(__resource__)
+
+# sys.path.append("C:\\Program Files\\Brainwy\\LiClipse 3.6.0\\plugins\\org.python.pydev_5.7.0.201704111135\\pysrc")
+# import pydevd
+# pydevd.settrace('localhost', port=34099, stdoutToServer=True, stderrToServer=True, suspend=False)
+
+from NapiProjekt import NapiProjektKatalog
+
+def log(msg=None, ex=None):
+    if ex:
+        level = xbmc.LOGERROR
+        msg = traceback.format_exc()
+    else:
+        level = xbmc.LOGINFO
+
+    xbmc.log((u"### [%s] - %s" % (__scriptname__, msg)), level=level)
+
+
+def Search(item):
+    filename = '.'.join(os.path.basename(item["file_original_path"]).split(".")[:-1])
+    helper = NapiProjektKatalog()
+    log("A:")
+    log(item)
+    results = helper.search(item)
+
+    for result in results:
+        listitem = xbmcgui.ListItem(label=xbmc.convertLanguage(result["language"], xbmc.ENGLISH_NAME),
+                                    # language name for the found subtitle
+                                    label2=result['label'],  # file name for the found subtitle
+                                    # language flag, ISO_639_1 language + gif extention, e.g - "en.gif"
+                                    )
+        listitem.setArt({'icon': "5", 'thumb': xbmc.convertLanguage(result["language"], xbmc.ISO_639_1)})
+        if result["sync"]:
+            listitem.setProperty("sync", "true")
+
+        # # below arguments are optional, it can be used to pass any info needed in download function
+        # # anything after "action=download&" will be sent to addon once user clicks listed subtitle to download
+        url = "plugin://%s/?action=download&l=%s&f=%s&filename=%s" % (
+            __scriptid__, result["language"], result['link_hash'], filename)
+        # # add it to list, this can be done as many times as needed for all subtitles found
+        xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=url, listitem=listitem, isFolder=False)
+
+
+def Download(language, hash, filename):
+    subtitle_list = []
+    # # Cleanup temp dir, we recomend you download/unzip your subs in temp folder and
+    # # pass that to XBMC to copy and activate
+    if xbmcvfs.exists(__temp__):
+        shutil.rmtree(__temp__)
+    xbmcvfs.mkdirs(__temp__)
+
+    filename = os.path.join(__temp__, filename + ".zip")
+    napiHelper = NapiProjektKatalog()
+    filename = napiHelper.download(hash, filename, language)
+    subtitle_list.append(filename)  # this can be url, local path or network path.
+
+    return subtitle_list
+
+
+def normalizeString(title):
+    try:
+        return str(''.join(
+            c for c in unicodedata.normalize('NFKD', convert(title)) if unicodedata.category(c) != 'Mn')).replace('ł',
+                                                                                                                  'l')
+    except:
+        title = convert(title).replace('ą', 'a').replace('ę', 'e').replace('ć', 'c').replace('ź', 'z').replace('ż',
+                                                                                                               'z').replace(
+            'ó', 'o').replace('ł', 'l').replace('ń', 'n').replace('ś', 's')
+        return title
+
+
+def convert(data):
+    if isinstance(data, bytes):
+        return data.decode()
+    if isinstance(data, (str, int)):
+        return str(data)
+    if isinstance(data, dict):
+        return dict(map(convert, data.items()))
+    if isinstance(data, tuple):
+        return tuple(map(convert, data))
+    if isinstance(data, list):
+        return list(map(convert, data))
+    if isinstance(data, set):
+        return set(map(convert, data))
+
+
+def get_params():
+    param = []
+    paramstring = sys.argv[2]
+    if len(paramstring) >= 2:
+        params = paramstring
+        cleanedparams = params.replace('?', '')
+        if (params[len(params) - 1] == '/'):
+            params = params[0:len(params) - 2]
+        pairsofparams = cleanedparams.split('&')
+        param = {}
+        for i in range(len(pairsofparams)):
+            splitparams = {}
+            splitparams = pairsofparams[i].split('=')
+            if (len(splitparams)) == 2:
+                param[splitparams[0]] = splitparams[1]
+
+    return param
+
+
+def fill_item_from_name(name, item):
+    try:
+        tv = re.findall(r"""(.*)          # Title
+                            [ .]
+                            (?:S|s)(\d{1,2})    # Season
+                            (?:E|e)(\d{1,2})    # Episode
+                            [ .a-zA-Z]*  # Space, period, or words like PROPER/Buried
+                            (\d{3,4}p)?   # Quality
+                        """, name, re.VERBOSE | re.IGNORECASE)
+        if len(tv) > 0:
+            item['tvshow'] = tv[0][0].replace(".", " ")
+            item['season'] = str(int(tv[0][1]))
+            item['episode'] = str(int(tv[0][2]))
+        else:
+            movie = re.findall(r"""(.*?[ .]\d{4})  # Title including year
+                                   [ .a-zA-Z]*     # Space, period, or words
+                                   (\d{3,4}p)?      # Quality
+                                """, name, re.VERBOSE)
+            if len(movie) > 0:
+                title = movie[0][0].replace(".", " ")
+                if len(title) > 4:
+                    year = try_read_year(title)
+                    if (year):
+                        item['year'] = year
+                        title = title[:-4].strip()
+                item['title'] = title
+            else:
+                item['title'] = name
+    except Exception as e:
+        pass
+
+
+def try_read_year(title):
+    try:
+        year = title[-4:]
+        return str(int(year))
+    except:
+        pass
+
+
+params = get_params()
+log(params)
+# params = {'action': 'search', 'languages': 'English%2cPolish', 'preferredlanguage': 'Polish'}
+
+if params['action'] == 'search' or params['action'] == 'manualsearch':
+    item = {}
+    item['temp'] = False
+    item['rar'] = False
+    item['year'] = xbmc.getInfoLabel("VideoPlayer.Year")  # Year
+    item['season'] = str(xbmc.getInfoLabel("VideoPlayer.Season"))  # Season
+    item['episode'] = str(xbmc.getInfoLabel("VideoPlayer.Episode"))  # Episode
+    item['tvshow'] = normalizeString(xbmc.getInfoLabel("VideoPlayer.TVshowtitle"))  # Show
+    item['videoplayer_title'] = normalizeString(xbmc.getInfoLabel("VideoPlayer.Title"))  # try to get original title
+    item['title'] = normalizeString(xbmc.getInfoLabel("VideoPlayer.OriginalTitle"))  # try to get original title
+    item['file_original_path'] = urllib.parse.unquote(
+        xbmc.Player().getPlayingFile())  # Full path of a playing file
+    item["file_original_size"] = xbmcvfs.File(item["file_original_path"]).size()
+
+    log(item)
+
+    item['3let_language'] = []
+    item['preferredlanguage'] = urllib.parse.unquote(params.get('preferredlanguage', ''))
+    item['preferredlanguage'] = xbmc.convertLanguage(item['preferredlanguage'], xbmc.ISO_639_2)
+
+    for lang in urllib.parse.unquote(params['languages']).split(","):
+        item['3let_language'].append(xbmc.convertLanguage(lang, xbmc.ISO_639_2))
+
+    possible_file = False
+    if item['title'] == "":
+        possible_file = True
+        item['title'] = normalizeString(xbmc.getInfoLabel("VideoPlayer.Title"))  # no original title, get just Title
+
+    if item['episode'].lower().find("s") > -1:  # Check if season is "Special"
+        item['season'] = "0"  #
+        item['episode'] = item['episode'][-1:]
+
+    if item['file_original_path'].find("http") > -1:
+        item['temp'] = True
+
+    elif item['file_original_path'].find("rar://") > -1:
+        item['rar'] = True
+        item['file_original_path'] = os.path.dirname(item['file_original_path'][6:])
+
+    elif item['file_original_path'].find("stack://") > -1:
+        stackPath = item['file_original_path'].split(" , ")
+        item['file_original_path'] = stackPath[0][8:]
+
+    
+
+    log("SEARCHING")
+
+
+     # if search string defined try filling data from search string
+    if params['action'] == 'manualsearch' and params.get('searchstring'):
+        log("SEARCHING MANUAL")
+        item['tvshow'] = ""
+        possible_file = False
+        search_string = urllib.parse.unquote(params['searchstring'])
+        log(search_string)
+
+        fill_item_from_name(search_string, item)
+    else:
+        # if no metadata then load from file
+        if possible_file and item['file_original_path']:
+            log("NO METADATA")
+            file_name = os.path.basename(item['file_original_path'])
+            if file_name == item['title']:
+                file_name = os.path.splitext(file_name)[0]
+                fill_item_from_name(file_name, item)
+
+    # item = {'temp': False, 'rar': False, 'year': '2017', 'season': '6', 'episode': '4', 'tvshow': 'Scandal (2012)', 'videoplayer_title': 'The Belt', 'title': 'The Belt', 'file_original_path': 'nfs://192.168.120.6/mnt/user/Seriale/Scandal (2012)/Scandal.2012.S06E04.HD.TV-AVS.mkv', 'file_original_size': 1077465459, '3let_language': ['eng', 'pl'], 'preferredlanguage': 'pl'}
+    log(item)
+
+    Search(item)
+
+
+elif params['action'] == 'download':
+    # # we pickup all our arguments sent from def Search()
+    subs = Download(params["l"], params["f"], params["filename"])
+    # # we can return more than one subtitle for multi CD versions, for now we are still working out how to handle that in XBMC core
+    for sub in subs:
+        listitem = xbmcgui.ListItem(label=sub)
+        xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=sub, listitem=listitem, isFolder=False)
+
+xbmcplugin.endOfDirectory(int(sys.argv[1]))  # # send end of directory to XBMC
